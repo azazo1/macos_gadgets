fc0f7c9df0b004af0b8b18255f170cdb4487c24c
azazo1 <azazo1@qq.com>
2025-12-01 11:19:28 +0800

    feat: watch_downloads ignore specific exts.

--- gadgets/watch_downloads/src/main.rs
+++ gadgets/watch_downloads/src/main.rs
@@ -7,6 +7,9 @@ use watch_downloads::{MDirEntry, cpcb_file};
 fn main() {
     tracing_subscriber::fmt().init();
 
+    let mut ignore_exts = HashSet::new();
+    ignore_exts.insert("aria2");
+
     info!("watch_downloads launched.");
     let download_dir = dirs::download_dir().unwrap();
     let mut file_set: HashSet<MDirEntry> = HashSet::new();
@@ -20,6 +23,11 @@ fn main() {
             cur_files.sort_by_key(|x| x.metadata().unwrap().accessed().unwrap());
             for f in &cur_files {
                 if !file_set.contains(&f.try_into().unwrap()) {
+                    if let Some(s) = f.path().extension()
+                        && ignore_exts.contains(s.to_string_lossy().to_string().as_str())
+                    {
+                        continue;
+                    }
                     cpcb_file(f.path()).unwrap();
                     info!("copy: {}", f.path().to_string_lossy());
                     break;
